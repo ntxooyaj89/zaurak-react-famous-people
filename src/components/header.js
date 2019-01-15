2525dcf297670acf7aa757dbcfd0f3ad74417458
Tang Yang <ty4081097@gmail.com>
2019-01-15 15:33:17 -0600

    added header file to project.

---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        // return something to render
+        return (
+            // the header is a closing tag so it will work.
+            <header className="App-header">
+                <h1>Famous People App</h1>
+            </header>
+
+        );
+    }
+
+}
+
+
+export default Header;
