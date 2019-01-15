031299aefeb234f92391c9900ff87d919a8743f4
Tang Yang <ty4081097@gmail.com>
2019-01-15 15:58:21 -0600

    add inputs and set the state.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/header';
+import FamousPerson from './components/FamousPerson'
 
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
       <Header />
+      <FamousPerson />
         
       </div>
     );
---
+++ src/components/FamousPerson.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+
+class FamousPerson extends Component {
+
+    // Called when the component is created
+    constructor() {
+        // super help start up the components
+        // super will only be used if we extends Components
+        super();
+
+        // if state changes it trigger another re-render
+        this.state = { name: 'Tom', role: 'Supervisor' }; // Luke isn't being used...
+    }
+
+    handleName = (event) => {
+        console.log(event.target.value);
+
+        // state only happens once if re-used you'll have to setState...
+        //this will set the state
+        this.setState({
+            // event.target is the input field
+           name: event.target.value
+          
+            //role: event.target.value
+        });
+
+        
+    }
+
+    handleRole = (event) => {
+        console.log(event.target.value);
+
+        this.setState({
+            // event.target is the input field
+
+           role: event.target.value
+        
+        });
+
+
+    }
+
+
+
+    render(){
+        return(
+            <div>
+                <input onChange = {this.handleName}
+                       type="text" placeholder= "name" />
+
+                <input onChange = {this.handleRole}
+                       type="text" placeholder="role" /> 
+
+            <br /> 
+            <p>This is name: {this.state.name}</p> 
+            <p>This is role: {this.state.role}</p>              
+
+            </div>
+        )
+    }
+}
+
+
+export default FamousPerson;
